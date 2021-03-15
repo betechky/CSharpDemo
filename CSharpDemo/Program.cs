65d9fa889fd80e594d2625901f631260fc238c1d
betechky <hjhdino@gmail.com>
2021-03-15 10:46:42 -0500

    Add project files.

---
+++ CSharpDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello .Net Core World!");
+        }
+    }
+}
